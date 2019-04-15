c5c13d2ea465e79ce7c4e934bc4e0d6cd34bbacd
Tanner Davis <tannerdavisr@gmail.com>
2019-04-15 16:28:22 -0600

    Luhn algorithm working

--- lib/credit_card_validator.dart
+++ lib/credit_card_validator.dart
@@ -139,5 +139,32 @@ class CreditCardValidator {
     return null;
   }
 
-  bool _isLuhnValid() {}
+  /// Checks the validity of the card number using the Luhn algorithm (the modulus 10 algorithm)
+  ///  For more info on Luhn algorithm check these URLS
+  ///     https://en.wikipedia.org/wiki/Luhn_algorithm
+  ///     https://www.geeksforgeeks.org/luhn-algorithm
+  ///
+  /// This method assumes that the incoming string is trimmed of whitespace
+  ///  and does not contain non-numerical characters. i.e.' -' or 'a-z'
+  bool _luhnValidity(String ccNum) {
+    int sum = 0;
+    bool alternate = false;
+
+    for (int i = ccNum.length - 1; i >= 0; i--) {
+      int digit = int.parse(ccNum[i]);
+
+      if (alternate) {
+        digit *= 2;
+        if (digit > 9) {
+          digit = (digit % 10) + 1;
+        }
+      }
+
+      sum += digit;
+
+      alternate = !alternate;
+    }
+
+    return sum % 10 == 0;
+  }
 }
