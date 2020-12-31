fa5879a8c65e50b6f23391c21514a22c8ce482c1
Tanner Davis <tannerdavisr@gmail.com>
2020-12-31 09:34:22 -0700

    Clean up imports

--- lib/credit_card_validator.dart
+++ lib/credit_card_validator.dart
@@ -1,17 +1,9 @@
-library credit_card_validator;
-
 import 'package:credit_card_type_detector/credit_card_type_detector.dart';
-import 'package:credit_card_validator/expiration_date.dart';
 
 import 'card_number.dart';
 import 'security_code.dart';
 import 'validation_results.dart';
-
-
-/// The default number of years into the future a card is valid. Set to 19
-/// i.e. if the current year is 2019 then a valid card would not have an expiration date greater than 2038
-const int DEFAULT_NUM_YEARS_IN_FUTURE = 19;
-
+import 'expiration_date.dart';
 
 /// [CreditCardValidator] helps with validating credit card numbers, expiration dates, and security codes.
 ///  It is meant to validate the credit card as the user is typing in the card information
