8737e0ce96b42893321bdf43050dca212b7bf20f
Tanner Davis <tannerdavisr@gmail.com>
2020-12-31 09:02:41 -0700

    Updated regexs for clarity

--- lib/regexs.dart
+++ lib/regexs.dart
@@ -1,19 +1,22 @@
 /// Contains regular expressions that are used across files
 
 /// Recognizes all whitespace characters
-RegExp whiteSpaceRegex = RegExp(r'-|\s+\b|\b\s');
+final RegExp whiteSpaceRegex = RegExp(r'-|\s+\b|\b\s');
 
 /// Recognizes all alphabet characters
-RegExp alphaCharsRegex = RegExp(r'[a-zA-Z]');
+final RegExp alphaCharsRegex = RegExp(r'[a-zA-Z]');
+
+/// Recognizes one or more non-numeric characters
+final RegExp nonNumberRegex = RegExp(r'\D+');
 
 /// Recognizes acceptable expiration date formats
 /// In plain english the steps are:
 ///       1) The month:
-///           a '0' followed by a number between '1' & '9 '
+///           a '0' followed by a number between '1' & '9 ' or just a number between '1' and '9'
 ///           OR
 ///           a '1' followed by a number between '0' & '2'
 ///       2) The slash:
 ///            a '/' (forward slash)
 ///       3) The year:
 ///           any combo of 2-4 numeric characters
-RegExp expDateFormat = RegExp(r'((0[1-9])|(1[0-2]))(/)+(\d{2,4})');
+final RegExp expDateFormat = RegExp(r'((0?[1-9])|(1[0-2]))(/)+(\d{2,4})');
